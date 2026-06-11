e1892ab5699136db069a6e695c799a4041260967
LucasJX <lucasjx@users.noreply.github.com>
2026-06-11 12:47:48 +0800

    fix: 添加 ExperimentalMaterial3Api 导入

--- app/src/main/java/com/flypigs/typechomanager/ui/home/HomeScreen.kt
+++ app/src/main/java/com/flypigs/typechomanager/ui/home/HomeScreen.kt
@@ -31,6 +31,7 @@ import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.outlined.Lightbulb
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
