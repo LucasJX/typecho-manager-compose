666ceca42608805d82410760d1d7afbabfa2a20c
flypigs <flypigs@users.noreply.github.com>
2026-06-12 09:33:49 +0800

    chore: bump version to 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flypigs.typechomanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.8.0"
+        versionCode = 14
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
