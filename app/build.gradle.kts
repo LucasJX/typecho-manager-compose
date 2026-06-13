31630ab906c57462e82ee241d9021e8f7006198b
flypigs <flypigs@users.noreply.github.com>
2026-06-13 21:46:41 +0800

    chore: bump version to 1.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flypigs.typechomanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.1.4"
+        versionCode = 16
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
