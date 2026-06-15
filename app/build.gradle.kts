aa19edc79d067b46b473b4582d53cf0c5171cde3
flypigs <flypigs@users.noreply.github.com>
2026-06-15 14:58:04 +0800

    chore: bump version to v1.3.0 (Reader Mode)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.flypigs.typechomanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.2.2"
+        versionCode = 24
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
